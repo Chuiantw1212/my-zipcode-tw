e7f798a099391901f4403de94da09c1ece7a6359
Chuian <chuiantw1212@gmail.com>
2019-05-03 23:23:23 +0800

    換成excel檔案

--- zipcode-tw/index.js
+++ zipcode-tw/index.js
@@ -1,10 +1,4 @@
-const fs = require('fs')
-fs.readFile('(10804)3+2郵遞區號UTF8.txt', 'utf8', (err, data) => {
-    if (err) {
-        throw err
-    }
-    const tokens = data.match(/[^\r\n]+/g);
-})
+
 
 exports.printMsg = function () {
     console.log("This is a message from the demo package");
