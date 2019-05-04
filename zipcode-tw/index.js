ddd49a14f2d718fd7a38394bd3f306e081b99c73
Chuian <chuiantw1212@gmail.com>
2019-05-04 18:19:52 +0800

    done with three post codes

--- zipcode-tw/index.js
+++ zipcode-tw/index.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 const fs = require('fs');
 // const path = require('path')
 function startTesting() {
-    const address = "台北市"
+    const address = "士林區"
     const zipcode = getPostCode(address);
     console.log({
         zipcode
@@ -42,15 +42,28 @@ async function initialize() {
 initialize();
 
 function getPostCode(targetAddress) {
-    const addressTokenGroup = targetAddress.match(/(?<zipcode>(^\d{5}|^\d{3})?)(?<city>\D+[縣市])(?<district>\D+?(市區|鎮區|鎮市|[鄉鎮市區]))(?<others>.+)/)
-    const { zipcode, city, district, others } = addressTokenGroup;
+    const TO_REPLACE_MAP = {
+        '-': '之', '~': '之', '台': '臺',
+        '１': '1', '２': '2', '３': '3', '４': '4', '５': '5',
+        '６': '6', '７': '7', '８': '8', '９': '9', '０': '0',
+        '一': '1', '二': '2', '三': '3', '四': '4', '五': '5',
+        '六': '6', '七': '7', '八': '8', '九': '9',
+    }
+    let correctAddress = targetAddress
+    for (let substitution in TO_REPLACE_MAP) {
+        const correctCharacter = TO_REPLACE_MAP[substitution]
+        correctAddress = correctAddress.replace(substitution, correctCharacter)
+    }
+    const matchResult = correctAddress.match(/(?<zipcode>(^\d{5}|^\d{3})?)(?<city>\D+[縣市])?(?<district>\D+?(市區|鎮區|鎮市|[鄉鎮市區]))?(?<others>.+)?/)
+    const { groups } = matchResult;
+    const { zipcode, city, district, others } = groups
     if (district) {
         const candidateAddress = allAddressOfTaiwan.find(address => {
             return address['鄉鎮市區'] === district
         })
         if (candidateAddress) {
             return Object.assign({}, {
-                zipcode: candidateAddress['郵遞區號'],
+                zipcode: candidateAddress['郵遞區號'].slice(0, 3),
                 city: candidateAddress['縣市名稱'],
                 district: candidateAddress['鄉鎮市區'],
             })
@@ -61,9 +74,8 @@ function getPostCode(targetAddress) {
         })
         if (candidateAddress) {
             return Object.assign({}, {
-                zipcode: candidateAddress['郵遞區號'].slice(0, 3),
+                zipcode: candidateAddress['郵遞區號'].slice(0, 1),
                 city: candidateAddress['縣市名稱'],
-                district: candidateAddress['鄉鎮市區'],
             })
         }
     } else if (zipcode) {
